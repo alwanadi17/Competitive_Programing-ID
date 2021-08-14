c376ce5bc28825923b98cb07ca365fc5bf1341bb
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2021-08-14 17:28:20 +0700

    Codewars/7_kyu_remove_duplicate_words: add ammarfaizi2's solution
    Add ammarfaizi2's solution written in C++.
    
    Link: https://t.me/CP_ID/402
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ Codewars/7_kyu_remove_duplicate_words/ammarfaizi2/solution.cpp
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2021  Ammar Faizi <ammarfaizi2@gmail.com>
+ */
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+std::string removeDuplicateWords(const std::string& str)
+{
+	std::unordered_map<std::string, bool> p;
+	std::string ret = "";
+	const char *c = str.c_str();
+	size_t start = 0, i, j = 0, len = str.length() + 1;
+
+	for (i = 0; i < len; i++) {
+		if (c[i] == ' ' || c[i] == '\0') {
+			std::string tmp(&c[start], i - start);
+			if (p.find(tmp) == p.end()) {
+				ret += (j++ ? " " : "") + tmp;
+				p[tmp] = true;
+			}
+			start = i + 1;
+		}
+	}
+	return ret;
+}
