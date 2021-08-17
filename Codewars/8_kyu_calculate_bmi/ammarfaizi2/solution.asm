09249abceea6ef3ac67726b5808c98782d68c6e2
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2021-08-17 14:29:02 +0700

    Codewars/8_kyu_calculate_bmi: add ammarfaizi2's solution in NASM x86-64
    Link: https://t.me/CP_ID/654
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ Codewars/8_kyu_calculate_bmi/ammarfaizi2/solution.asm
@@ -0,0 +1,52 @@
+; SPDX-License-Identifier: GPL-2.0
+;
+; Copyright (C) 2021  Ammar Faizi <ammarfaizi2@gmail.com>
+;
+
+[section .rodata]
+align 8
+_case1:
+	dq 18.5
+_case2:
+	dq 25.0
+_case3:
+	dq 30.0
+_str1:
+	db "Underweight",0
+_str2:
+	db "Normal",0
+_str3:
+	db "Overweight",0
+_str4:
+	db "Obese",0
+
+[section .text]
+global bmi
+; const char *__bmi(int, double);
+; // Weight is placed in EDI
+; // Height is placed in xmm0 - the number format is double
+__bmi:
+	cvtsi2sd	xmm1, edi
+	mulsd		xmm0, xmm0
+	divsd		xmm1, xmm0
+
+	lea		rax, [rel _str1]
+	movsd		xmm0, [rel _case1]
+	ucomisd		xmm1, xmm0
+	jbe		.l_out
+
+	lea		rax, [rel _str2]
+	movsd		xmm0, [rel _case2]
+	ucomisd		xmm1, xmm0
+	jbe		.l_out
+
+	lea		rax, [rel _str3]
+	movsd		xmm0, [rel _case3]
+	ucomisd		xmm1, xmm0
+	jbe		.l_out
+
+	lea		rax, [rel _str4]
+.l_out:
+	ret
+bmi:
+	jmp	__bmi
