40499a24f7e17f7049ea5144e9b17265d057d5aa
alpha-ai <85019398+alpha-ai@users.noreply.github.com>
2021-08-18 12:36:45 +0700

    Add solution by alpha

---
+++ Codewars/beta_make_upper/alpha/solution.cpp
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2021  Alwan Adiuntoro <alwanadiuntoro@gmail.com>
+ */
+
+char makeupper(char letter) {
+  return letter - 'a' + 'A';
+}
