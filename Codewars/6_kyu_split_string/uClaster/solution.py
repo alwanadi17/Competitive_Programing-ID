8eadc1357e2826197ed371c225bbc1a4cc798631
uClaster <uclasterdev@gmail.com>
2021-09-05 22:38:13 +0800

    Add license

--- Codewars/6_kyu_split_string/uClaster/solution.py
+++ Codewars/6_kyu_split_string/uClaster/solution.py
@@ -1,2 +1,8 @@
+'''
+SPDX-License-Identifier: GPL-2.0-only
+Copyright (C) 2021 uClaster <uclasterdev@gmail.com>
+'''
+
+
 def solution(s: str) -> list:
 	return [s[se:se+2] if len(s[se:se+2]) % 2 == 0 else s[se]+'_' for se in range(0, len(s), 2)]
