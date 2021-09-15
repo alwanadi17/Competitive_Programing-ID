00bbfc443eac5387582c2ca030e13421e12ca5f0
Ammar Faizi <ammarfaizi2@gmail.com>
2021-09-15 21:09:02 +0700

    Codewars/8_kyu_playing_with_cubes_I: add ammarfaizi2's solution in Java
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

---
+++ Codewars/8_kyu_playing_with_cubes_I/ammarfaizi2/Cube.java
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2021  Ammar Faizi <ammarfaizi2@gmail.com>
+ */
+public class Cube
+{
+	private int side = 0;
+
+	public void setSide(int num)
+	{
+		this.side = num;
+	}
+
+	public int getSide()
+	{
+		return this.side;
+	}
+}
