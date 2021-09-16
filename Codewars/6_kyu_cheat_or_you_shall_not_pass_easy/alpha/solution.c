6957946d8815b2ef7c3f3bb981e7d6dd1b40a8a1
Alwan Adiuntoro <85019398+alpha-ai@users.noreply.github.com>
2021-09-16 22:39:25 +0700

    Codewars/6_kyu_cheat_or_you_shall_not_pass_easy : Add Alwan's solution on C
    Signed-off-by: Alwan Adiuntoro <alwanadiuntoro@gmail.com>

---
+++ Codewars/6_kyu_cheat_or_you_shall_not_pass_easy/alpha/solution.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2021  Alwan Adiuntoro <alwanadiuntoro@gmail.com>
+ */
+
+int solution()
+{
+  srand(0); //set seed to fixed seed
+  return 383;
+}
