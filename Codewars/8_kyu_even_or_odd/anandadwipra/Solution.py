87af6f48d95be965a1f2563745fa8194c81f517e
anandadwipra <anandabiru04@gmail.com>
2021-10-20 15:08:14 +0700

    :rocket: Add New solution

---
+++ Codewars/8_kyu_even_or_odd/anandadwipra/Solution.py
@@ -0,0 +1,6 @@
+#  SPDX-License-Identifier: GPL-2.0-only
+# 
+#   Copyright (C) 2021  anandadwipra <anandabiru04@gmail.com> 
+
+def even_or_odd(number):
+    return "Even" if number%2==0 else "Odd"
---
+++ Codewars/8_kyu_hex_to_decimal/anandadwipra/Solution.py
@@ -0,0 +1,6 @@
+#  SPDX-License-Identifier: GPL-2.0-only
+# 
+#   Copyright (C) 2021  anandadwipra <anandabiru04@gmail.com> 
+
+def hex_to_dec(s):
+    return int(s,16)
