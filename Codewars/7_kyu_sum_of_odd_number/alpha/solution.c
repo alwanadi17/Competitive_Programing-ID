52ea20749d13299c7b92b64ac74fc7a154999c44
Alwan Adiuntoro <alwanadiuntoro@gmail.com>
2021-11-27 22:43:13 +0700

    Codewars/7_kyu_sum_of_odd_number: add Alwan's solution in C
    Signed-off-by: Alwan Adiuntoro <alwanadiuntoro@gmail.com>

---
+++ Codewars/7_kyu_sum_of_odd_number/alpha/solution.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2021  Alwan Adiuntoro <alwanadiuntoro@gmail.com>
+ */
+
+#include <inttypes.h>
+
+uint64_t rowSumOddNumbers(uint32_t n)
+{
+    return (uint64_t) n * n * n;
+}
