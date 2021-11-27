6c050fc93518ff011b800472e0b34fdcc358191c
Alwan Adiuntoro <alwanadiuntoro@gmail.com>
2021-11-27 22:49:48 +0700

    Codewars/6_kyu_multiplication_table: add Alwan's solution in C
    Signed-off-by: Alwan Adiuntoro <alwanadiuntoro@gmail.com>

---
+++ Codewars/6_kyu_multiplication_table/alpha/solution.c
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2021  Alwan Adiuntoro <alwanadiuntoro@gmail.com>
+ */
+
+#include <stdlib.h>
+
+int **multiplication_table(int n) {
+    int **ret = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++)
+      ret[i] = (int*)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < n; j++)
+        *(*(ret+i)+j) = (i+1)*(j+1);
+
+    return ret;
+}
